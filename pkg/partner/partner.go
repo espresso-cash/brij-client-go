d3cf0c92c86dea86eabc3fc8a05fb2a468413768
Kirill Bubochkin <ookami.kb@gmail.com>
2025-05-26 21:22:16 +0200

    refactor: update protos dependency and modify KYC data structures

--- pkg/partner/partner.go
+++ pkg/partner/partner.go
@@ -3,7 +3,6 @@ package partner
 import (
 	"context"
 	"crypto/ed25519"
-	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/btcsuite/btcutil/base58"
@@ -11,6 +10,7 @@ import (
 	"go.brij.fi/protos/brij/storage/v1/common"
 	"go.brij.fi/protos/brij/storage/v1/partner"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"go.brij.fi/client/internal/client"
 	"go.brij.fi/client/internal/encryption"
@@ -18,16 +18,10 @@ import (
 
 type Client interface {
 	GetData(ctx context.Context, userPK ed25519.PublicKey) (map[string]*ValidatedData, error)
-	SetValidationResult(
-		ctx context.Context,
-		dataId string,
-		user ed25519.PublicKey,
-		hash string,
-		status common.ValidationStatus,
-	) error
+	SetValidationResult(ctx context.Context, hash string, status common.ValidationStatus) error
 	Encrypt(ctx context.Context, user ed25519.PublicKey, data []byte) (encrypted []byte, hash string, err error)
-	CreateKycStatus(ctx context.Context, input *common.KycItem) (string, error)
-	GetKycStatus(ctx context.Context, input *GetKycStatusInput) (*partner.GetKycStatusResponse, error)
+	CreateKycStatus(ctx context.Context, input *common.KycEnvelope) (string, error)
+	GetKycStatus(ctx context.Context, input *GetKycStatusInput) (*common.KycEnvelope, error)
 	UpdateKycStatus(ctx context.Context, input *UpdateKycStatusInput) error
 	Close() error
 	PublicKey() ed25519.PublicKey
@@ -54,7 +48,7 @@ type GetKycStatusInput struct {
 
 type UpdateKycStatusInput struct {
 	KycID string
-	Data  *common.KycItem
+	Data  *common.KycEnvelope
 }
 
 func New(privateKey ed25519.PrivateKey, host string) (Client, error) {
@@ -91,7 +85,7 @@ func (c *kycPartnerClient) PublicKey() ed25519.PublicKey {
 
 func (c *kycPartnerClient) GetData(ctx context.Context, userPK ed25519.PublicKey) (map[string]*ValidatedData, error) {
 	result := map[string]*ValidatedData{}
-	validationMap := map[string]*common.ValidationDataField{}
+	validationMap := map[string]*common.ValidationDataEnvelope{}
 
 	rawData, err := c.apiClient.GetUserData(
 		ctx, connect.NewRequest(
@@ -111,50 +105,63 @@ func (c *kycPartnerClient) GetData(ctx context.Context, userPK ed25519.PublicKey
 	}
 
 	for _, v := range rawData.Msg.ValidationData {
-		validationMap[v.DataId] = v
+		var envelope common.ValidationDataEnvelope
+		if err := proto.Unmarshal(v.Payload, &envelope); err != nil {
+			return result, err
+		}
+
+		// TODO: Validate the signature of the envelope
+
+		validationMap[envelope.DataHash] = &envelope
 	}
 
 	for _, v := range rawData.Msg.UserData {
-		decrypted, err := encryption.DecryptUserData(sk, v.EncryptedValue)
+		var envelope common.UserDataEnvelope
+		if err := proto.Unmarshal(v.Payload, &envelope); err != nil {
+			return result, err
+		}
+
+		// TODO: Validate the signature of the envelope
+
+		decrypted, err := encryption.DecryptUserData(sk, envelope.EncryptedValue)
 		if err != nil {
 			return result, err
 		}
 
 		status := common.ValidationStatus_VALIDATION_STATUS_UNSPECIFIED
-		if validationData, exists := validationMap[v.Id]; exists {
+		if validationData, exists := validationMap[v.Hash]; exists {
 			status = validationData.Status
 		}
 
-		result[v.Id] = &ValidatedData{
+		result[v.Hash] = &ValidatedData{
 			UserData: decrypted,
 			Status:   status,
 			Hash:     v.Hash,
-			Type:     v.Type,
+			Type:     envelope.Type,
 		}
 	}
 
 	return result, nil
 }
 
-func (c *kycPartnerClient) SetValidationResult(
-	ctx context.Context,
-	dataId string,
-	user ed25519.PublicKey,
-	hash string,
-	status common.ValidationStatus,
-) error {
-	signature := encryption.SignMessage(
-		c.privateKey,
-		[]byte(fmt.Sprintf("%s|%s|%s|%s", dataId, base58.Encode(user), hash, status)),
-	)
+func (c *kycPartnerClient) SetValidationResult(ctx context.Context, hash string, status common.ValidationStatus) error {
+	envelope := &common.ValidationDataEnvelope{
+		DataHash:           hash,
+		ValidatorPublicKey: base58.Encode(c.publicKey),
+		Status:             status,
+		ValidatedAt:        timestamppb.Now(),
+	}
+	payload, err := proto.Marshal(envelope)
+	if err != nil {
+		return err
+	}
+	signature := encryption.SignMessage(c.privateKey, payload)
 
 	in := &partner.SetValidationDataRequest{
-		DataId:    dataId,
-		Status:    status,
-		Hash:      hash,
-		Signature: base58.Encode(signature),
+		Payload:   payload,
+		Signature: signature,
 	}
-	_, err := c.apiClient.SetValidationData(ctx, connect.NewRequest(in))
+	_, err = c.apiClient.SetValidationData(ctx, connect.NewRequest(in))
 
 	return err
 }
@@ -176,7 +183,7 @@ func (c *kycPartnerClient) Encrypt(
 	return encrypted, hash, nil
 }
 
-func (c *kycPartnerClient) CreateKycStatus(ctx context.Context, input *common.KycItem) (string, error) {
+func (c *kycPartnerClient) CreateKycStatus(ctx context.Context, input *common.KycEnvelope) (string, error) {
 	data, err := proto.Marshal(input)
 	if err != nil {
 		return "", err
@@ -185,7 +192,7 @@ func (c *kycPartnerClient) CreateKycStatus(ctx context.Context, input *common.Ky
 	signature := encryption.SignMessage(c.privateKey, data)
 
 	in := &partner.CreateKycStatusRequest{
-		Data:      data,
+		Payload:   data,
 		Signature: signature,
 	}
 
@@ -198,7 +205,7 @@ func (c *kycPartnerClient) CreateKycStatus(ctx context.Context, input *common.Ky
 }
 
 func (c *kycPartnerClient) GetKycStatus(ctx context.Context, input *GetKycStatusInput) (
-	*partner.GetKycStatusResponse,
+	*common.KycEnvelope,
 	error,
 ) {
 	in := &partner.GetKycStatusRequest{
@@ -212,7 +219,14 @@ func (c *kycPartnerClient) GetKycStatus(ctx context.Context, input *GetKycStatus
 		return nil, err
 	}
 
-	return resp.Msg, nil
+	var envelope common.KycEnvelope
+	if err := proto.Unmarshal(resp.Msg.Payload, &envelope); err != nil {
+		return nil, err
+	}
+
+	// TODO: Validate the signature of the envelope
+
+	return &envelope, nil
 }
 
 func (c *kycPartnerClient) UpdateKycStatus(ctx context.Context, input *UpdateKycStatusInput) error {
@@ -225,7 +239,7 @@ func (c *kycPartnerClient) UpdateKycStatus(ctx context.Context, input *UpdateKyc
 
 	in := &partner.UpdateKycStatusRequest{
 		KycId:     input.KycID,
-		Data:      data,
+		Payload:   data,
 		Signature: signature,
 	}
 
